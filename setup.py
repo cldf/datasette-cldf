875e25ebc0093713933154c980d68fe61e6aa69f
xrotwang <xrotwang@googlemail.com>
2020-02-14 11:06:39 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datasette-cldf',
-    version='0.2.0',
+    version='0.2.1.dev0',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='datasette plugin to explore CLDF datasets',
